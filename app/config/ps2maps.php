b012136b10dd26439e572cd80c7af5ace104ec3b
Jake Wilson <jake.wilson@gunsightlabs.com>
2014-06-01 22:54:29 -0600

    ps2maps config created

--- app/config/ps2maps.php
+++ app/config/ps2maps.php
@@ -2,5 +2,7 @@
 
 return array(
 
+	'title' => "PlanetSide 2 Maps - ps2maps.com - Interactive maps for Amerish, Esamir and Indar",
+	'meta_description' => "PlanetSide 2 Maps is the definitive resource for interactive Auxaris continent maps - Amerish, Esamir and Indar",
 
 );
