b4e0cbd289957cbcbb09d057efb9721906807ee9
Jake Wilson <jake.wilson@gunsightlabs.com>
2014-06-01 22:54:06 -0600

    Moved to their own views

--- app/views/opengraph.blade.php
+++ app/views/opengraph.blade.php
@@ -1,5 +1,5 @@
 <meta property="og:site_name" content="PlanetSide 2 Maps"/>
-<meta property="og:title" content="{{ $title }}" />
-<meta property="og:url" content="{{ $url }}" />
+<meta property="og:title" content="@yield('title', Config::get('ps2maps.title'))" />
+<meta property="og:url" content="{{ Request::url() }}" />
 <meta property="og:image" content="/img/logo-square.png" />
 <meta property="og:type" content="website" />
--- app/views/twitter.blade.php
+++ app/views/twitter.blade.php
@@ -1,7 +1,7 @@
 <meta name="twitter:card" content="summary">
-<meta name="twitter:url" content="{{ $url }}">
-<meta name="twitter:title" content="{{ $title }}">
-<meta name="twitter:description" content="{{ $metaDescription }}">
+<meta name="twitter:url" content="{{ Request::url() }}">
+<meta name="twitter:title" content="@yield('title', Config::get('ps2maps.title'))">
+<meta name="twitter:description" content="@yield('meta_description', Config::get('ps2maps.meta_description'))">
 <meta name="twitter:image" content="/img/logo-square.png">
 <meta name="twitter:site" content="@ps2maps">
 <meta name="twitter:creator" content="@ps2maps">
