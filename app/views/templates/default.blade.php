b8832b6287dd07938ee643d6f517598c8fa636ad
Jake Wilson <jake.wilson@gunsightlabs.com>
2014-06-01 23:13:18 -0600

    Moved template view(s)

--- app/views/templates/default.blade.php
+++ app/views/templates/default.blade.php
@@ -1,10 +1,4 @@
-<?php
-
-$title = isset($title) ? $title : "PlanetSide 2 Maps - ps2maps.com - Interactive maps for Amerish, Esamir and Indar";
-$metaDescription = isset($metaDescription) ? $metaDescription : "PlanetSide 2 Maps is the definitive resource for interactive Auxaris continent maps - Amerish, Esamir and Indar";
-$url = Request::url();
-
-?><!DOCTYPE html>
+<!DOCTYPE html>
 <!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]><html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]><html class="no-js lt-ie9"> <![endif]-->
@@ -12,8 +6,8 @@ $url = Request::url();
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<title>{{ $title }}</title>
-	<meta name="description" content="{{ $metaDescription }}">
+	<title>@yield('title', Config::get('ps2maps.title'))</title>
+	<meta name="description" content="@yield('meta_description', Config::get('ps2maps.meta_description'))">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 	@include('opengraph')
@@ -22,11 +16,10 @@ $url = Request::url();
 	<script type='text/javascript'>if (top !== self) top.location.href = self.location.href;</script>
 
 	<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
-	<link rel="canonical" href="{{ $url }}" />
-	<link href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet" media="screen"/>
+	<link rel="canonical" href="{{ Request::url() }}" />
+	<link href="/css/bootstrap.css" rel="stylesheet" media="screen"/>
 	<link href="/css/main.css" rel="stylesheet" type="text/css" charset="utf-8"/>
 	<link href='http://fonts.googleapis.com/css?family=Roboto+Condensed' rel='stylesheet' type='text/css'>
-
 	@yield('styles')
 
 	<script src='/js/modernizr.custom.js'></script>
@@ -34,23 +27,24 @@ $url = Request::url();
 	<script>
 	var server, continent = null;
 	</script>
+
 	@yield('head')
 </head>
 <body>
 
-	@include('menu.menu')
+	@include('menu')
 
 	@yield('content')
 
 	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
 	<script>window.jQuery || document.write('<script src="js/jquery-2.1.0.min.js"><\/script>')</script>
 	<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.1.1/js/bootstrap.min.js"></script>
+	@yield('scripts');
 
 	<script>
 		@yield('javascript')
 
-		$('document').ready(function()
-		{
+		$(function() {
 			@yield('jquery')
 		});
 	</script>
