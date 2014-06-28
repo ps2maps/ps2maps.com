be959d2dd811f3dd6fa9a2d5f8c4df8300c3cf23
Jake Wilson <jake.wilson@gunsightlabs.com>
2014-06-27 19:31:24 -0600

    Updated Server model for Census syncing

--- app/models/Server.php
+++ app/models/Server.php
@@ -2,47 +2,12 @@
 
 class Server extends Eloquent
 {
+	public $incrementing = false;
+
 	public $fillable = array(
+		'id',
 		'name',
-		'census_server_id',
-		'status'
+		'slug',
+		'status',
 	);
-
-	public static function getAll()
-	{
-		return Cache::get('servers/all', function()
-		{
-			$servers = Server::where_enabled('yes')
-				->order_by('name')
-				->get();
-
-			if ( empty($servers) )
-				return $servers;
-
-			// Cache Result
-			Cache::put('servers/all', $servers, 1440);
-
-			return $servers;
-		});
-	}
-
-	public function getOtherServers()
-	{
-		$server = $this;
-
-		return Cache::get('servers/all_except_'.$this->slug, function() use ($server)
-		{
-			$servers = Server::where_enabled('yes')
-							->where('id','!=',$server->id)
-							->order_by('name')
-							->get();
-
-			if ( empty($servers) )
-				return $servers;
-
-			Cache::put('servers/all_except_'.$server->slug, $servers, 1440);
-
-			return $servers;
-		});
-	}
 }
