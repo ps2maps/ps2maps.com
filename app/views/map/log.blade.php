cad2082586e21c15065ac83bc2e34d3bcbe098f5
Jake Wilson <jake.wilson@gunsightlabs.com>
2014-07-05 22:04:45 -0600

    Initial map log/history view

---
+++ app/views/map/log.blade.php
@@ -0,0 +1,16 @@
+<div class='log'>
+	<div class='log-resize-handle'></div>
+	<div class='header'>
+		Connery Indar
+	</div>
+	<ul class='history'>
+		<li>Whatever was taken</li>
+		<li>Whatever was taken</li>
+		<li>Whatever was taken</li>
+		<li>Whatever was taken</li>
+		<li>Whatever was taken</li>
+		<li>Whatever was taken</li>
+		<li>Whatever was taken</li>
+		<li>Whatever was taken</li>
+	</ul>
+</div>
