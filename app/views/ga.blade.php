25477196038a32b8783f0f35d02950ad0a28cc0c
Jake Wilson <jake.wilson@gunsightlabs.com>
2014-07-05 22:01:12 -0600

    Commented out GA and SC during dev

--- app/views/ga.blade.php
+++ app/views/ga.blade.php
@@ -7,17 +7,17 @@
 		_gaq.push(['_trackEvent', '15_seconds', 'read']);
 	},15000);
 
-	(function() {
-		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	})();
+	// (function() {
+	// 	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	// 	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	// 	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	// })();
 
 	// StatCounter
 	var sc_project=8473421;
 	var sc_invisible=1;
 	var sc_security="8617036a";
 	var scJsHost = (("https:" == document.location.protocol) ? "https://secure." : "http://www.");
-	document.write("<sc"+"ript type='text/javascript' src='" + scJsHost + "statcounter.com/counter/counter.js' async='true'></"+"script>");
+	//document.write("<sc"+"ript type='text/javascript' src='" + scJsHost + "statcounter.com/counter/counter.js' async='true'></"+"script>");
 </script>
 <noscript><div class="statcounter"><a title="web analytics" href="http://statcounter.com/" target="_blank"><img class="statcounter" src="https://c.statcounter.com/8473421/0/8617036a/1/"	alt="web analytics"></a></div></noscript>
