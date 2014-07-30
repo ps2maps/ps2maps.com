911b7a853403981622d527448537857e4a835a5b
Jake Wilson <jake.wilson@gunsightlabs.com>
2014-07-29 20:46:45 -0600

    Default custom colors

--- app/config/ps2maps.php
+++ app/config/ps2maps.php
@@ -6,4 +6,13 @@ return array(
 	'meta_description' => "PlanetSide 2 Maps is the definitive resource for interactive Auxaris continent maps - Amerish, Esamir and Indar",
 
 	'tiles-cdn' => '//cdn{s}.ps2maps.com',
+
+	'faction-colors' => [
+		'ns' => '#79e0e1',
+		'nc' => '#33ADFF',
+		'tr' => '#DF2020',
+		'vs' => '#9E52E0',
+	],
+
+	'time-format' => 'h:mm:ss a',
 );
