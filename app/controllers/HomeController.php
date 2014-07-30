0d28013efe2408cb140a4797014376e6b2bd874d
Jake Wilson <jake.wilson@gunsightlabs.com>
2014-07-29 20:46:30 -0600

    Settings controller methods

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -11,5 +11,23 @@ class HomeController extends BaseController {
 		return View::make('home/indexnew', compact('servers'));
 	}
 
+	public function getSettings()
+	{
+		return View::make('map/settings');
+	}
+
+	public function postSettings()
+	{
+		// Save colors to session
+		if ( Input::has('nc-color') )
+			Session::put('faction-colors.nc',Input::get('nc-color'));
+		if ( Input::has('tr-color') )
+			Session::put('faction-colors.tr',Input::get('tr-color'));
+		if ( Input::has('vs-color') )
+			Session::put('faction-colors.vs',Input::get('vs-color'));
+
+		return Redirect::to('/settings')->with('message', 'Settings Saved');
+	}
+
 }
 
