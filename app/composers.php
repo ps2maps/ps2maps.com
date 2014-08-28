9f795eafcfc10704dcd6e1368b7a11fae505b105
Jake Wilson <jake.wilson@gunsightlabs.com>
2014-08-27 18:19:44 -0600

    Various composer changes. Added Session-set faction colors to view composer.

--- app/composers.php
+++ app/composers.php
@@ -5,23 +5,26 @@ $views = array(
 	'menu',
 	'home/index',
 	'server/index',
+	'server/continent',
 );
 View::composer($views, function($view)
 {
-	$servers = Server::orderBy('name')
-		->whereEnabled('yes')
+	// Get all servers
+	$servers = Server::whereEnabled('yes')
+		->orderBy('name')
 		->remember(1440)
-		->get();
+		->get()
+		->getDictionary();
 
 	$view->with('servers', $servers);
 });
 
-
 // Get all enabled continents
 $views = array(
 	'menu',
-	'server/index',
 	'home/indexnew',
+	'server/index',
+	'server/continent',
 );
 View::composer($views, function($view)
 {
@@ -29,7 +32,39 @@ View::composer($views, function($view)
 		->whereEnabled('yes')
 		->where('slug','!=','vRTraining')
 		->remember(1440)
-		->get();
+		->get()
+		->getDictionary();
 
 	$view->with('continents', $continents);
+});
+
+// Get Session Server
+$views = [
+	'menu',
+];
+View::composer($views, function($view)
+{
+	// Get the current session server, default to Connery
+	$server = Session::get('server', function(){
+		return Server::where('slug','=','connery')
+			->remember(1440)
+			->first();
+	});
+
+	$view->with('sessionServer', $server);
+});
+
+// Get Faction Colors
+$views = [
+	'server/index',
+	'server/continent',
+];
+View::composer($views, function($view)
+{
+	$factionColors = [
+		'nc' => Session::get('faction-colors.nc', Config::get('ps2maps.faction-colors.nc')),
+		'tr' => Session::get('faction-colors.tr', Config::get('ps2maps.faction-colors.tr')),
+		'vs' => Session::get('faction-colors.vs', Config::get('ps2maps.faction-colors.vs')),
+	];
+	$view->with('factionColors', $factionColors);
 });
