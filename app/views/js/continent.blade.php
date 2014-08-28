986bcb5b883acd446cbec1d3eb2fc8be2e484777
Jake Wilson <jake.wilson@gunsightlabs.com>
2014-08-27 18:18:36 -0600

    Added hardcoded continent attributes.

--- app/views/js/continent.blade.php
+++ app/views/js/continent.blade.php
@@ -1,6 +1,9 @@
 // {{ $continent->name }} - Generated: {{ $carbon->toDateTimeString() }} {{ $carbon->timezoneName }}
 
 var {{ $continent->slug }} = {
+	id: {{ $continent->id }},
+	name: "{{ $continent->name }}",
+	slug: "{{ $continent->slug }}",
 	regions : {{ $regions }},
 	markers : {
 		facilities: {{ $facilities }}
