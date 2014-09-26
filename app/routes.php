87c92e903fa71c97a6f91b79fe6278bdf066063f
Jake Wilson <jake.wilson@gunsightlabs.com>
2014-09-25 22:37:57 -0600

    Changed HomeController to more generic PageController

--- app/routes.php
+++ app/routes.php
@@ -6,11 +6,12 @@ Route::get('blog/{slug}', 'BlogController@article');
 Route::get('blog', 'BlogController@index');
 
 // Settings
-Route::get('settings', 'HomeController@getSettings');
-Route::post('settings', 'HomeController@postSettings');
+Route::get('settings', 'PageController@getSettings');
+Route::post('settings', 'PageController@postSettings');
 
 // Servers and Continents
-Route::get('embed/{server}/{continent}', 'ServerController@embed');
+// Route::get('embed/{server}/{continent}', 'ServerController@embed');
+// Route::get('lock-status/{server_id}/{continent_id}', 'ServerController@isLocked');
 Route::get('{server}/{continent}', 'ServerController@continent');
 Route::get('{server}', 'ServerController@server');
 
