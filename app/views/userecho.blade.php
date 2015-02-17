5ef778290eb31677823b534fa9fc6d603a389c18
Jake Wilson <jake.e.wilson@gmail.com>
2015-02-16 21:06:11 -0700

    Uncommented the Userecho Feedback script

--- app/views/userecho.blade.php
+++ app/views/userecho.blade.php
@@ -15,11 +15,11 @@ tab_bg_color:'#57A957',
 tab_hover_color:'#F45C5C'
 };
 
-// (function() {
-//   var _ue = document.createElement('script'); _ue.type = 'text/javascript'; _ue.async = true;
-//   _ue.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.userecho.com/js/widget-1.4.gz.js';
-//   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(_ue, s);
-// })();
+(function() {
+  var _ue = document.createElement('script'); _ue.type = 'text/javascript'; _ue.async = true;
+  _ue.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.userecho.com/js/widget-1.4.gz.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(_ue, s);
+})();
 
 </script>
-{{--<a onmouseover="UE.Popin.preload();" href="#" onclick="UE.Popin.show(); return false;">feedback</a>--}}
+<a onmouseover="UE.Popin.preload();" href="#" onclick="UE.Popin.show(); return false;">feedback</a>
