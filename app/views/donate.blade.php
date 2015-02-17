2eebda2487e0082004819288b144d7f19de01c77
Jake Wilson <jake.e.wilson@gmail.com>
2015-02-17 00:25:33 -0700

    Initial work on Donation page

---
+++ app/views/donate.blade.php
@@ -0,0 +1,24 @@
+@extends('templates/default')
+
+
+@section('content')
+
+<div class='container'>
+
+	<div class='jumbotron'>
+		<h1>Help Support PlanetSide 2 Maps</h1>
+	</div>
+
+	<div class='row'>
+
+		<div class="col-sm-12">
+
+			<p>
+				PlanetSide 2 Maps gets a fair amount of traffic from passionate PlanetSide 2 players. The site takes a fair amount of online resources
+			</p>
+
+		</div>
+
+</div>
+
+@stop
