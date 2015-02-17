5dc2376b2a03cb9ace6c28c3b68808ec519f1bcf
Jake Wilson <jake.e.wilson@gmail.com>
2015-02-16 20:02:43 -0700

    Removed a buncn of old unused views

--- app/views/sidebar/feedback.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="accordion-group">
-				<div class="accordion-heading" id='user-echo-button'>
-					<a href='http://ps2maps.userecho.com' target="ps2maps.userecho.com" class="accordion-toggle">
-						<i class='icon-comment icon-white'></i> FEEDBACK
-					</a>
-				</div>
-			</div>
--- app/views/sidebar/server.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="accordion-group">
-	<div class="accordion-heading">
-		@if ( isset($server) ){{ $server->name }} - @endif{{ $continent->name }}
-	</div>
-</div>
