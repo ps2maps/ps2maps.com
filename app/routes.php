c384508ea336f66675571338af3f0f6b2fae0f11
Jake Wilson <jake.e.wilson@gmail.com>
2015-03-08 12:48:55 -0600

    Removed blog routes. Added embeddable maps route

--- app/routes.php
+++ app/routes.php
@@ -1,21 +1,35 @@
 <?php
 
-// Blog
-Route::get('blog/rss', 'BlogController@rss');
-Route::get('blog/{slug}', 'BlogController@article');
-Route::get('blog', 'BlogController@index');
+// Route::group(['before'=>'auth'], function(){
+// 	Route::controller('admin', 'AdminController');
+// });
+
+// Fetch search sources
+Route::post('searchSources', 'PageController@postSearchSources');
+
+// Route::get('donate', function(){
+// 	return View::make('donate');
+// });
+//
+// Route::get('test', function(){
+// 	return View::make('embed_test');
+// });
+
+// Embedding instructions
+Route::get('embeddable', 'PageController@getEmbeddable');
 
 // Settings
 Route::get('settings', 'PageController@getSettings');
 Route::post('settings', 'PageController@postSettings');
 
 // Servers and Continents
-// Route::get('embed/{server}/{continent}', 'ServerController@embed');
-// Route::get('lock-status/{server_id}/{continent_id}', 'ServerController@isLocked');
-Route::get('{server}/{continent}', 'ServerController@continent');
-Route::get('{server}', 'ServerController@server');
+Route::get('{server}/{continent}/embed', 'MapController@embed');
+Route::get('{server}/{continent}', 'MapController@continent');
+Route::get('{server}', 'MapController@server');
+
+
 
 // Home
-Route::get('/', 'HomeController@getIndex');
+Route::get('/', 'PageController@getIndex');
 
 
